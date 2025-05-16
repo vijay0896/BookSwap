d41c2cc74d49e80d397a32094dd37141676bc73e
Vijay Choudhary <Vijaychoudhary6059@gmail.com>
2025-05-16 15:26:15 +0530

    Server js file

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
+const server = app;
+const PORT = process.env.PORT;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Worker ${process.pid} is running on port ${PORT}`);
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 });
