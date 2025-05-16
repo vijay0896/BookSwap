bda8ec90f4af077eec64ecef80e60904e0427b55
Vijay Choudhary <Vijaychoudhary6059@gmail.com>
2025-05-16 13:14:50 +0530

    Added clusters in server

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const app = require("./app");
+const cluster = require('cluster');
+const os = require('os');
 
-const server = app;
+const PORT = process.env.PORT || 3000;
 
-// Start server
-const PORT = process.env.PORT;
-server.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+if (cluster.isMaster) {
+  const cpuCount = os.cpus().length;
+  console.log(`Master ${process.pid} is running`);
+  console.log(`Forking ${cpuCount} workers...`);
+
+  for (let i = 0; i < cpuCount; i++) {
+    cluster.fork();
+  }
+
+  // Optional: Restart a worker if it crashes
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died. Forking a new one.`);
+    cluster.fork();
+  });
+
+} else {
+  // Workers share the same server port
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Worker ${process.pid} is running on port ${PORT}`);
+  });
+}
