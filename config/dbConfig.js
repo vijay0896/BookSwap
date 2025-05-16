d3adde2e1bd7806be6d67b56ec183449fb3123e4
Vijay Choudhary <Vijaychoudhary6059@gmail.com>
2025-05-16 15:48:48 +0530

    dbConfig

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST || "127.0.0.1",
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
